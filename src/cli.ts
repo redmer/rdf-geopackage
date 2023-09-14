ca2c4bf0cdbea6fe2126be71c2a4b2c0cbb789c4
Redmer Kronemeijer <12477216+redmer@users.noreply.github.com>
2023-09-14 15:41:30 +0200

    feat: limit features to a bounding box
    partially resolves #1

--- src/cli.ts
+++ src/cli.ts
@@ -2,23 +2,16 @@
 
 import { StreamWriter } from "n3";
 import { createWriteStream } from "node:fs";
-import { Writable } from "node:stream";
 import { pathToFileURL } from "node:url";
 import streamifyArray from "streamify-array";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+import {
+  WGS84_CODE,
+  getWGS84Converter,
+  suppliedBoundingBox,
+} from "./bounding-box.js";
 import { quadsFromGeoPackage } from "./rdf-geopackage.js";
-
-/** Generate the RDF quads from the GeoPackage and direct to out */
-export async function run(filepath: string, mimetype: string, out: Writable) {
-  const quadStream = streamifyArray(await quadsFromGeoPackage(filepath));
-  const writer = new StreamWriter({ mimetype });
-
-  quadStream
-    .pipe(writer)
-    .pipe(out)
-    .on("error", (error) => console.error(error));
-}
 
 async function cli() {
   const argv = await yargs(hideBin(process.argv))
@@ -26,14 +19,41 @@ async function cli() {
     .demandOption("input")
     .option("output", { alias: "o", type: "string", desc: "Output quads file" })
     .normalize(["input", "output"])
+    .option("bounding-box", {
+      type: "string",
+      desc: "Output features from this bounding box",
+    })
+    .option("bounding-box-crs", {
+      type: "string",
+      desc: `Supply a CRS code (default: ${WGS84_CODE})`,
+      default: WGS84_CODE,
+    })
+    .option("only-layers", { type: "array" })
     .parse();
+
+  // If there's a bounding box CRS defined, first check if we can parse it.
+  // This is less expensive than converting quads etc.
+  const converter = argv.boundingBoxCrs
+    ? await getWGS84Converter(argv.boundingBoxCrs)
+    : WGS84_CODE;
+  const boundingBox = argv.boundingBox
+    ? suppliedBoundingBox(argv.boundingBox, converter)
+    : undefined;
 
   const target = argv.output
     ? createWriteStream(argv.output, { encoding: "utf-8" })
     : process.stdout;
   const mimetype = "application/n-quads";
 
-  await run(argv.input, mimetype, target);
+  const quadStream = streamifyArray(
+    await quadsFromGeoPackage(argv.input, boundingBox),
+  );
+  const writer = new StreamWriter({ mimetype });
+
+  quadStream
+    .pipe(writer)
+    .pipe(target)
+    .on("error", (error) => console.error(error));
 }
 
 void cli();
